4fc31a9c4ed8b235aa9284ae82b5d7f2a15cc539
Faizaan Alam <faizaanalam.connect@gmail.com>
2026-09-13 03:38:55 +0530

    fix: open Team at Meet the team with this year's four only
    #team

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Layout() {
   return (
     <div className="flex min-h-svh flex-col">
+      <ScrollToTop />
       <Navbar />
       <main id="main" className="flex-1">
         <Outlet />
